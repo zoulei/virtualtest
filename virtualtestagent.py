543f4500758abae4121595d249eaa8324f783c49
zoul15 <346954742@qq.com>
2017-11-10 13:47:11 +0800

    sync

--- virtualtestagent.py
+++ virtualtestagent.py
@@ -5,5 +5,9 @@ class VirtualTestAgent(handsengine):
         handsengine.__init__(self,handsinfo)
         self.m_pokeragent = pokeragent
 
+    def updatecumuinfo(self,round,actionidx):
+        self.m_cumuinfo
+        handsengine.updatecumuinfo(self,round,actionidx)
+
     def test(self):
         pass

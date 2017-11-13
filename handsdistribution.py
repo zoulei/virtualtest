95a63d4803bd56dc6c1ee6eb59764d432db15266
leizou <346954742@qq.com>
2017-11-13 18:20:05 +0800

    sync

--- handsdistribution.py
+++ handsdistribution.py
@@ -1,7 +1,25 @@
 
 class HandsDisQuality:
-    def __init__(self,dis):
-        pass
+    def __init__(self,dis = None):
+        if dis is None:
+            self.m_handsdis = {}
+        else:
+            self.m_handsdis = dis
+
+    def __add__(self, other):
+        for key in self.m_handsdis.keys():
+            self.m_handsdis[key] += other.m_handsdis.get(key,0)
+        for key in other.m_handsdis.keys():
+            if key in self.m_handsdis:
+                continue
+            self.m_handsdis[key] = other.m_handsdis[key]
+        return self
+
+    # make the sum of probability be 1
+    def normalize(self):
+        total = sum(self.m_handsdis.values())
+        for key in self.m_handsdis.keys():
+            self.m_handsdis[key] /= total
 
     def calquality(self):
         pass
---
+++ rullbasedagent.py
@@ -0,0 +1,99 @@
+from hunlgame import HandsRange
+
+class HonestAgent:
+    # game acts like a dealer
+    # pos is from 1-9, 9 is sb, 8 is bb
+    def __init__(self, dealer, pos = 0, myhand=None):
+        self.m_dealer = dealer
+        self.m_pos = pos
+        self.m_myhand = myhand
+
+        self.m_winrateengine = None
+        self.m_distribution = {}
+
+        self.m_lastactionturn = 0
+
+    # init my hands distribution based on my strategy
+    # this distribution can be used to determine my hand
+    # based on my strategy and action.
+    def initdistribution(self):
+        handrangeobj = HandsRange()
+        handslist = handrangeobj.get()
+        self.m_distribution = dict(zip(handslist, [1.0/len(handslist)]*len(handslist)))
+
+    # update my hands distribution based on new public card
+    def updatedistributionbyboard(self):
+        board = self.m_dealer.getboard()
+
+        # eliminate hands that is impossible according to public card
+        if self.m_lastactionturn != self.m_dealer.getcurrentturn():
+            if self.m_dealer.getcurrentturn == 2:
+                # flop
+                for hand in self.m_distribution.keys():
+                    for publiccard in board:
+                        if publiccard in hand.get():
+                            self.m_distribution[hand] = 0
+                            break
+            elif self.m_dealer.getcurrentturn > 2:
+                # turn and river
+                for hand in self.m_distribution.keys():
+                    if board[-1] in hand.get():
+                        self.m_distribution[hand] = 0
+
+        # normalize probability
+        rate = 1.0 / sum(self.m_distribution.values())
+        for hand in self.m_distribution.keys():
+            self.m_distribution[hand] *= rate
+
+    # update distribution based on my action
+    # action is 0 : fold, 1 : call, 2 : raise
+    def updatedistributionbyaction(self, action, value):
+        mother = 0
+
+        responsedata = {}
+        for hand,prob in self.m_distribution.items():
+            if prob == 0:
+                continue
+            responsedata[hand] = self.getresponse(hand)
+            mother += self.getresponse(hand)[action] * prob
+
+        for hand,prob in self.m_distribution.items():
+            if prob == 0:
+                continue
+            self.m_distribution[hand] = prob * responsedata[hand][action] / mother
+
+    # make action
+    def act(self, action, value):
+        self.updatedistributionbyboard()
+        self.m_lastactionturn = self.m_dealer.getcurrentturn()
+        self.m_dealer.updateaction(action,value)
+        self.updatedistributionbyaction(action, value)
+
+    # ask dealer for hand
+    def askforhand(self):
+        hand = self.m_dealer.distributehand(self.m_pos)
+        self.sethand(hand)
+
+    # set my hand
+    def sethand(self,hand):
+        self.m_myhand = hand
+
+    # my response under current game state if my hand is virtualhand
+    # if virtualhand is not given, then use my real hand
+    # return tuple, represents (fold, call or check, raise)
+    def getresponse(self, virtualhand = None):
+        if virtualhand is None:
+            virtualhand = self.m_myhand
+        self.m_winrateengine = hunlgame.FPWinrateEngine(self.m_dealer.getboard(), virtualhand)
+        winrate = self.m_winrateengine.calmywinrate()
+        isnuts = self.m_winrateengine.isnuts()
+        if isnuts:
+            return (0,0,1)
+        raiserate = winrate * winrate * winrate
+        callrate = winrate * winrate
+        foldrate = 1 - raiserate - callrate
+        if self.m_dealer.checkavailable():
+            return (0, foldrate + callrate, raiserate)
+        else:
+            return (foldrate,callrate, raiserate)
+
